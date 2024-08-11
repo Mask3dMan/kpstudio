a5a377261f1a4190f6e2e08da7d14e36bc53ac51
Mateusz Cabizza <m4tt94@gmail.com>
2024-08-11 11:50:10 +0200

    moved to custom domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@
 let a = 16 // gradi di rotazione
 let random
 const inv = -1
+let inscroll = false
 document.addEventListener('DOMContentLoaded', function() {
 {% for item in site.data.nav%}
   random = a/2 - Math.floor(Math.random()*a)
@@ -13,30 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('neg-{{item.name}}').style.transform = `rotate(${inv*(random)}deg)`
 {% endfor %}
 
-{% for category in site.data.categories %}
-const {{category.name | split: " " | first | prepend: 'btn'}} = document.getElementById('{{category.name}}')
-{% endfor %}
+// {% for category in site.data.categories %}
+// const {{category.name | split: " " | first | prepend: 'btn'}} = document.getElementById('{{category.name}}')
+// {% endfor %}
 
-function handleRadioClick() {
+// function handleRadioClick() {
 
-{% for category in site.data.categories %}
-if ({{category.name | split: " " | first | prepend: 'btn'}}.checked) {
-  document.getElementById('group{{category.name}}').classList.remove('hidden');
-}
-else {
-  document.getElementById('group{{category.name}}').classList.add('hidden');
-}
+// {% for category in site.data.categories %}
+// if ({{category.name | split: " " | first | prepend: 'btn'}}.checked) {
+//   document.getElementById('group{{category.name}}').classList.remove('hidden');
+// }
+// else {
+//   document.getElementById('group{{category.name}}').classList.add('hidden');
+// }
 
-{% endfor %}
+// {% endfor %}
 
-}
+// }
 
-const radioButtons = document.querySelectorAll(
-  'input[name="radio"]',
-);
+// const radioButtons = document.querySelectorAll(
+//   'input[name="radio"]',
+// );
 
-radioButtons.forEach(radio => {
-  radio.addEventListener('click', handleRadioClick);
-});
+// radioButtons.forEach(radio => {
+//   radio.addEventListener('click', handleRadioClick);
+// });
 });
 
