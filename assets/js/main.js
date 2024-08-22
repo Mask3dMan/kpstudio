edc0c3aab136f25611f3cbab4d161d7686256667
icodeonsunday <icodeonsunday@users.noreply.github.com>
2024-08-22 14:43:51 +0000

    deploy: 49180c214b6d80f42b26f72137ebc2d09edd12ff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let a = 16 // gradi di rotazione
+let a = 16 // max rotation degrees
 let random
 const inv = -1
 let inscroll = false
@@ -21,80 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('neg-Contact').style.transform = `rotate(${inv*(random)}deg)`
 
 
-// 
-// const btnAll = document.getElementById('All')
-// 
-// const btn3D = document.getElementById('3D')
-// 
-// const btnicons = document.getElementById('icons')
-// 
-// const btnbrand = document.getElementById('brand identity')
-// 
-// const btnlogos = document.getElementById('logos')
-// 
-// const btnillustrations = document.getElementById('illustrations')
-// 
 
-// function handleRadioClick() {
-
-// 
-// if (btnAll.checked) {
-//   document.getElementById('groupAll').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('groupAll').classList.add('hidden');
-// }
-
-// 
-// if (btn3D.checked) {
-//   document.getElementById('group3D').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('group3D').classList.add('hidden');
-// }
-
-// 
-// if (btnicons.checked) {
-//   document.getElementById('groupicons').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('groupicons').classList.add('hidden');
-// }
-
-// 
-// if (btnbrand.checked) {
-//   document.getElementById('groupbrand identity').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('groupbrand identity').classList.add('hidden');
-// }
-
-// 
-// if (btnlogos.checked) {
-//   document.getElementById('grouplogos').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('grouplogos').classList.add('hidden');
-// }
-
-// 
-// if (btnillustrations.checked) {
-//   document.getElementById('groupillustrations').classList.remove('hidden');
-// }
-// else {
-//   document.getElementById('groupillustrations').classList.add('hidden');
-// }
-
-// 
-
-// }
-
-// const radioButtons = document.querySelectorAll(
-//   'input[name="radio"]',
-// );
-
-// radioButtons.forEach(radio => {
-//   radio.addEventListener('click', handleRadioClick);
-// });
 });
 
